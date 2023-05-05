521a6529f5c274fda1a5d70a6aa2926897053bce
ernman37 <Rion.duckworth@gmail.com>
2023-05-04 22:29:37 -0600

    Add a little styling to header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import { Anchor, Nav, Box, Header, Menu, ResponsiveContext } from 'grommet';
+import { Anchor, Nav, Box, Header, Avatar, Text } from 'grommet';
 import { Grommet as GrommetIcon, Menu as MenuIcon } from 'grommet-icons';
+import Ernest from '../photos/Ernest-Duckworth.jpeg';
 
 export const AppHeader = () => (
   <Header background="light-3" pad="small">
-    <Box alignSelf="center">
-      Ernests Website
+    <Box direction='row' alignSelf="center">
+      <Avatar src={Ernest}/>
+      <Text alignSelf="center" margin="small">
+        Ernests Website
+      </Text>
     </Box>
     <Nav direction="row">
       <Anchor label="Experience" href="experience" />
