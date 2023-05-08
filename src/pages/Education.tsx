68aa9040496096a0c99012bb767fda40018c35b3
ernman37 <Rion.duckworth@gmail.com>
2023-05-07 20:24:05 -0600

    Add routing and other pages

---
+++ src/pages/Education.tsx
@@ -0,0 +1,18 @@
+/*
+ *    File: Education.ts
+ *    Creator: Ernest M Duckworth IV
+ *    Created: Sunday May 07 2023 at 05:32:34 PM
+ *    For: 
+ *    Description:
+*/
+import {Box} from "grommet";
+import {AppHeader} from "../components/AppHeader";
+
+export default function Education(){
+
+    return(
+        <Box height="84vh" width='100vw' overflow="auto">
+            Education Page
+        </Box>
+    );
+}
